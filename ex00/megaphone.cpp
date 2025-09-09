ebd1174667336290bf3cae825e4266403bab4406
Lumilu99 <yenwen_lu@hotmail.com>
2025-09-09 17:02:04 +0800

    Initialize: First CPP00 commit

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yelu <yelu@student.42kl.edu.my>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/08 15:27:56 by yelu              #+#    #+#             */
+/*   Updated: 2025/09/08 16:27:01 by yelu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+			{
+				std::cout << (char)std::toupper(argv[i][j]);
+			}
+			if (i < argc -1)
+				std::cout << " ";
+		}
+		std::cout << "\n";
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	return (0);
+}
+
