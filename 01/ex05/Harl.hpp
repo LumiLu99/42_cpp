f8e786932b80dadfb6f29a0c5b7287ed0ef0cb3c
Lumilu99 <yenwen_lu@hotmail.com>
2025-12-06 18:22:35 +0800

    Feat: Completed CP01

---
+++ 01/ex05/Harl.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yelu <yelu@student.42kl.edu.my>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/04 14:10:15 by yelu              #+#    #+#             */
+/*   Updated: 2025/12/04 14:11:49 by yelu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <iostream>
+
+class Harl
+{
+    private:
+        void debug(void);
+        void info(void);
+        void warning(void);
+        void error(void);
+    public:
+        void complain(std::string level);
+};
+
+#endif
