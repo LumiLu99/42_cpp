bd67bd3dda2d5fda7b81bfdc622586c97bbebf25
Lumilu99 <yenwen_lu@hotmail.com>
2026-07-28 18:25:03 +0800

    Feat: Hpp and tpp files

---
+++ 08/ex02/MutantStack.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yelu <yelu@student.42kl.edu.my>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/28 14:25:42 by yelu              #+#    #+#             */
+/*   Updated: 2026/07/28 16:54:11 by yelu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+	public:
+		MutantStack();
+		MutantStack(const MutantStack &other);
+		MutantStack &operator=(const MutantStack &other);
+		~MutantStack();
+
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+	iterator begin();
+	iterator end();
+	const_iterator begin() const;
+	const_iterator end() const;
+};
+
+#include "MutantStack.tpp"
+
+#endif
